cb38ad6704d92c274c5373c91983f66c6aa2d7f2
Solera <jsolera@demo.es>
2019-06-18 18:14:18 +0000

    Creacion de cookbook

---
+++ recipes/server.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: myiis
+# Recipe:: server
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+powershell_script 'Intall IIS' do
+  code 'Add-WindowsFeature Web-Server'
+end
+
+file 'c:\inetpub\wwwroot\Default.htm' do
+  content '<h1>Hello, world!</h1> Yeah'
+end
+
+service 'w3svc' do
+  action [:enable, :start]
+end
